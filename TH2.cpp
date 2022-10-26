976440223883e5b0760f1100588156618f835e3f
minhut <minhuta1@gmail.com>
2022-10-26 09:40:36 +0700

    Them ham nhap xuat

---
+++ TH2.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>;
+int A[100];
+void nhapmang ( int A[100], int n);
+void xuatmang ( int A[100], int n);
+int main()
+{
+	int n;
+	printf ("nhap so phan tu cho mang la mot so nguyen :"); scanf("%d",&n);
+	nhapmang(A,n);
+}
+
+
+
+
+void nhapmang (int A[100], int n)
+{
+	printf("\n\n Nhap gia tri cac phan tu cho mang A :");
+	int i;
+	for (i=0;i<n;i++)
+	{
+		printf("\n\n Nhap gia tri cho phan tu A[%d]=",i); scanf("%d", &A[i]);
+	}
+}
+
+void xuatmang ( int A[100], int n)
+{
+	printf("\n\n Xuat gia tri cac phan tu cho mang A :");
+	int i;
+	for (i=0;i<n;i++)
+	{
+		printf("\n\nGia tri cua phan tu A[%d]=",i)
+	}
+
+}
